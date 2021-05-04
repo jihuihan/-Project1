39d135d648e0db8db1222146e32f5cd063e0665e
jihui <2188449@naver.com>
2021-05-04 17:21:19 +0900

    전송

--- Project1/src/com/javaex/basics/Ex.java
+++ Project1/src/com/javaex/basics/Ex.java
@@ -8,6 +8,8 @@ public class Ex {
 		star();//별 프로그램 실행
 	}//ex1구구단 프로그램
 	private static void multi() {
+		
+		System.out.println("구구단 프로그램입니다.");
 		for (int i = 2; i < 10; i++) {
 			for (int j = 1; j < 10; j++) {
 				System.out.println(i+ "*"+j+ " = " +i*j);
@@ -17,15 +19,18 @@ public class Ex {
 		
 	}
 	private static void star() {
-		for (int i = 1; i < 6; i++) {
-			System.out.println("*");
-			for (int j = 0; j < i; j++) {
+		System.out.println("==================================================================================================");
+		System.out.println("별 프로그램입니다.");
+		for (int i = 0; i <=6; i++) {
+			for (int j = 0; j <= i; j++) {
 				System.out.print("*");
 			}
-			}
 			System.out.println();
+			}
+			
 		}
 	}
+
 	
 	
 	
